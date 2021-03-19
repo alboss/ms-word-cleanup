f8b6149c837b111ac50c8987a7c85f25c8034e53
Al Boss <alboss@sisypheans.org>
2021-03-18 18:04:41 -0700

    Update MSWordHtmlCleaners.js

--- MSWordHtmlCleaners.js
+++ MSWordHtmlCleaners.js
@@ -1,8 +1,8 @@
 var cleaningArea = document.getElementById('cleaning').value;
 var CleanWordHTML = function ( str )
 {
-str = str.replace(/<o:p>\s*<\/o:p>/g, "") ;
-str = str.replace(/<o:p>.*?<\/o:p>/g, "&nbsp;") ;
+str = str.replace(/<o:p>\s*<\/o:p>/g, "<p>\s*<\/p>") ;
+str = str.replace(/<o:p>.*?<\/o:p>/g, "<p>\s*<\/p>") ;
 str = str.replace( /\s*mso-[^:]+:[^;"]+;?/gi, "" ) ;
 str = str.replace( /\s*MARGIN: 0cm 0cm 0pt\s*;/gi, "" ) ;
 str = str.replace( /\s*MARGIN: 0cm 0cm 0pt\s*"/gi, "\"" ) ;
@@ -26,14 +26,20 @@ str = str.replace( /<SPAN\s*>(.*?)<\/SPAN>/gi, '$1' ) ;
 str = str.replace( /<FONT\s*>(.*?)<\/FONT>/gi, '$1' ) ;
 str = str.replace(/<\\?\?xml[^>]*>/gi, "") ; 
 str = str.replace(/<\/?\w+:[^>]*>/gi, "") ; 
-str = str.replace( /<H\d>\s*<\/H\d>/gi, '' ) ;
-str = str.replace( /<H1([^>]*)>/gi, '' ) ;
-str = str.replace( /<H2([^>]*)>/gi, '' ) ;
-str = str.replace( /<H3([^>]*)>/gi, '' ) ;
-str = str.replace( /<H4([^>]*)>/gi, '' ) ;
-str = str.replace( /<H5([^>]*)>/gi, '' ) ;
-str = str.replace( /<H6([^>]*)>/gi, '' ) ;
-str = str.replace( /<\/H\d>/gi, '<br>' ) ; //remove this to take out breaks where Heading tags were 
+//str = str.replace( /<H\d>\s*<\/H\d>/gi, '' ) ;
+str = str.replace( /<H1([^>]*)>/gi, '<h1>' ) ;
+str = str.replace( /<H2([^>]*)>/gi, '<h2>' ) ;
+str = str.replace( /<H3([^>]*)>/gi, '<h3>' ) ;
+str = str.replace( /<H4([^>]*)>/gi, '<h4>' ) ;
+str = str.replace( /<H5([^>]*)>/gi, '<h5>' ) ;
+str = str.replace( /<H6([^>]*)>/gi, '<h6>' ) ;
+str = str.replace( /<\/H1>/gi, '<h1>' ) ;
+str = str.replace( /<\/H2>/gi, '<h2>' ) ;
+str = str.replace( /<\/H3>/gi, '<h3>' ) ;
+str = str.replace( /<\/H4>/gi, '<h4>' ) ;
+str = str.replace( /<\/H5>/gi, '<h5>' ) ;
+str = str.replace( /<\/H6>/gi, '<h6>' ) ;
+    //str = str.replace( /<\/H\d>/gi, '<br>' ) ; //remove this to take out breaks where Heading tags were 
 str = str.replace( /<(U|I|STRIKE)>&nbsp;<\/\1>/g, '&nbsp;' ) ;
 str = str.replace( /<(B|b)>&nbsp;<\/\b|B>/g, '' ) ;
 str = str.replace( /<([^\s>]+)[^>]*>\s*<\/\1>/g, '' ) ;
@@ -50,10 +56,39 @@ return str ;
 cleaningArea = str;
 }
 
+function cleanHTML(input) {
+    // 1. remove line breaks / Mso classes
+    var stringStripper = /(\n|\r| class=(")?Mso[a-zA-Z]+(")? ^p)/g;
+    var output = input.replace(stringStripper, ' ');
 
+    // 2. strip Word generated HTML comments
+    var commentSripper = new RegExp('<!--(.*?)-->','g');
+    var output = output.replace(commentSripper, '');
+
+    // 3. remove tags leave content if any
+    var tagStripper = new RegExp('<(/)*(meta|link|span|\\?xml:|st1:|o:|font)(.*?)>','gi');
+    output = output.replace(tagStripper, '');
+
+    // 4. Remove everything in between and including tags '<style(.)style(.)>'
+    var badTags = ['style', 'script','applet','embed','noframes','noscript'];
+
+    for (var i=0; i< badTags.length; i++) {
+        tagStripper = new RegExp('<'+badTags[i]+'.*?'+badTags[i]+'(.*?)>', 'gi');
+        output = output.replace(tagStripper, '');
+    }
+
+    // 5. remove attributes ' style="..."'
+    var badAttributes = ['style', 'start'];
+    for (var i=0; i< badAttributes.length; i++) {
+        var attributeStripper = new RegExp(' ' + badAttributes[i] + '="(.*?)"','gi');
+        output = output.replace(attributeStripper, '');
+    }
+    return output;
+}
 
 var cleanPaste = function(html) {
     // Run the standard YUI cleanHTML method
+    // not useful since we don't use the YUI editor
     html = this.Editor.cleanHTML(html);
 
     // Remove additional MS Word content
