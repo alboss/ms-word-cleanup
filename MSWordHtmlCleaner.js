cfd550fb232154a2e73556957550c55d3450f157
Al Boss <alboss@sisypheans.org>
2021-03-24 18:49:30 -0700

    Replace b with strong

--- MSWordHtmlCleaner.js
+++ MSWordHtmlCleaner.js
@@ -65,7 +65,7 @@ new Vue({
         .replace(/<p>(\s)*(<br \/>)+/g, '<p>')
         // use html5 <br> instead of xhtml <br />
         .replace(/<br\s*\/?>/ig, '<br>')
-        // <br /><br /> -> </p><p>
+        // <b> -> <strong>
         .replace(/<b>(.*?)<\/b>/g, '<strong>$1</strong>');
       ;
       // delete empty tags
