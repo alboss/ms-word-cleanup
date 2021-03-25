4ffecf46a0b50f12479fe14f7626dfaeb6f14c4e
Al Boss <alboss@sisypheans.org>
2021-03-24 18:22:40 -0700

    Update MSWordHtmlCleaner.js

--- MSWordHtmlCleaner.js
+++ MSWordHtmlCleaner.js
@@ -3,7 +3,8 @@
 
 const allowedTags = [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
   'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
-  'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre' ];
+  /*'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td',*/ 'pre' ];
+  // No tables!
 
 const deleteEmptyTags = [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
   'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'div',
